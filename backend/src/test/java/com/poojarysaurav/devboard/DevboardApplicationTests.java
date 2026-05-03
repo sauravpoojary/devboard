6b62b9c1ec07bfab30d9d479ae7c744ab74de22f
Saurav Poojary <sauravpoojary@gmail.com>
2026-05-03 12:34:48 +0530

    initial commit

---
+++ backend/src/test/java/com/poojarysaurav/devboard/DevboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poojarysaurav.devboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
